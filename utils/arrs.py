4c2da7dd9e4ab81d5dbf9c46d3576c3ff1248bd5
имя фамилия <kralkin1@mail.ru>
2024-02-04 14:47:32 +0300

    Покрытие 100

--- utils/arrs.py
+++ utils/arrs.py
@@ -8,12 +8,9 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-
     if 0 <= index < len(array):
         return array[index]
     return default
-
-
 def my_slice(coll, start=None, end=None):
     """
     Возвращает новый массив, содержащий копию части исходного массива.
@@ -24,20 +21,17 @@ def my_slice(coll, start=None, end=None):
     Если индекс отрицательный, end указывает смещение от конца списка. По умолчанию равен длине исходного списка.
     :return: массив элементов
     """
-
     length = len(coll)
-
     if length == 0:
         return []
 
-    if start is not None:
-        normalized_start = start
+    if start is None:
+        normalized_start = 0
     else:
-        normalized_start = None
+        normalized_start = start
 
     if end is None or end > length:
         normalized_end = length
     else:
         normalized_end = end
-
     return coll[normalized_start:normalized_end]
